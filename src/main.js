6fa5a5363a721620163b67f14a1382bf91d5f9f5
nczkevin <z1376871863@163.com>
2017-02-28 22:45:00 +0800

    首页header

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  // template: '<App/>',
+  // components: { App }
+  render: h => h(App)
 })
