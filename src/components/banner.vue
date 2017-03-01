5e270d147e3aa61d7f87d459c2c1d228a3a55559
nczkevin <z1376871863@163.com>
2017-03-01 11:26:14 +0800

    top

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    
+    <div class="slider-banner">
+      <a href="http://dushu.xiaomi.com/#page=topic&type=fiction&id=11439&_t=index.0.sld.0&&from=main">
+        <img class="cover" src="http://t1.market.xiaomi.com/thumbnail/jpeg/l640/Duokan/0e7a88448e66d41e803d3b6b276c04e48911c788c?" alt="">
+      </a>
+    </div>
   </div>
 </template>
 
@@ -17,4 +21,7 @@ export default {
 </script>
 
 <style lang="css">
+  .cover {
+    width: 100%;
+  }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,12 +2,40 @@
   <div>
     <v-header></v-header>
     <v-search></v-search>
+    <v-banner></v-banner>
+    <ul class="top_btn">
+      <li>
+        <router-link to="male">
+          <img class="top_icon" src="../assets/male.png" alt="">
+          <p class="top_desc">男生</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="female">
+          <img class="top_icon" src="../assets/female.png" alt="">
+          <p class="top_desc">女生</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="category">
+          <img class="top_icon" src="../assets/category.png" alt="">
+          <p class="top_desc">分类</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="rank">
+          <img class="top_icon" src="../assets/rank.png" alt="">
+          <p class="top_desc">排行</p>
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
   import header from '../components/header.vue'
   import search from '../components/search.vue'
+  import banner from '../components/banner.vue'
 
   export default {
     data () {
@@ -18,10 +46,28 @@
     methods: {},
     components: {
       'v-header': header,
-      'v-search': search
+      'v-search': search,
+      'v-banner': banner
     }
   }
 </script>
 
 <style lang="css">
+  .top_btn {
+    margin: 14px 0 4px;
+    display: -webkit-box;
+    width: 100%;
+  }
+  .top_btn li {
+    line-height: 2.4;
+    font-size: 12px;
+    text-align: center;
+    -webkit-box-flex: 1;
+  }
+  .top_icon {
+    display: block;
+    margin: 0 auto;
+    height: 22px;
+    width: 22px;
+  }
 </style>
